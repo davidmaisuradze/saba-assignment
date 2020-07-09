db9b691a57e69301262754cb57ec3b456506b0e3
David Maisuradze <dmaisuradze05231995@gmail.com>
2020-07-10 01:34:30 +0400

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PizzaAppModule } from './pizza-app/pizza-app.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    PizzaAppModule
   ],
   providers: [],
   bootstrap: [AppComponent]
